8b2e0e841d791e29903e58bd33ab297c3271ef3a
Antonio Barone <syntonyze@gmail.com>
2025-12-23 13:00:24 +0100

    Refactor ApplyObject to use reusable BatchRefUpdate builder
    The ref update is still executed as a BatchRefUpdate containing a single
    ReceiveCommand, but the logic that inserts objects and adds commands is
    now factored out in preparation for multi-ref batch execution.
    
    No behavioral changes are intended in this step.
    
    Bug: Issue 456926963
    Change-Id: Ib3c7d046954303824371405a2ae9818fb5c9b203

--- src/main/java/com/gerritforge/gerrit/plugins/replication/pull/fetch/ApplyObject.java
+++ src/main/java/com/gerritforge/gerrit/plugins/replication/pull/fetch/ApplyObject.java
@@ -13,10 +13,8 @@ package com.gerritforge.gerrit.plugins.replication.pull.fetch;
 
 import com.gerritforge.gerrit.plugins.replication.pull.LocalGitRepositoryManagerProvider;
 import com.gerritforge.gerrit.plugins.replication.pull.api.data.RevisionData;
-import com.gerritforge.gerrit.plugins.replication.pull.api.data.RevisionObjectData;
 import com.gerritforge.gerrit.plugins.replication.pull.api.exception.MissingLatestPatchSetException;
 import com.gerritforge.gerrit.plugins.replication.pull.api.exception.MissingParentObjectException;
-import com.google.common.base.Optional;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.restapi.IdString;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
@@ -25,13 +23,7 @@ import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.inject.Inject;
 import java.io.IOException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.lib.BatchRefUpdate;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectInserter;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.transport.ReceiveCommand;
 import org.eclipse.jgit.transport.RefSpec;
 
 public class ApplyObject {
@@ -54,61 +46,11 @@ public class ApplyObject {
           ResourceNotFoundException,
           MissingLatestPatchSetException {
     try (Repository git = gitManager.openRepository(name)) {
+      BatchApplyObject batch = BatchApplyObject.create(git);
+      batch.add(name, refSpec, revisionsData);
+      RefUpdateUtil.executeChecked(batch.getBatchRefUpdate(), git);
+      return new BatchRefUpdateState(batch.getBatchRefUpdate());
 
-      ObjectId refHead = null;
-      BatchRefUpdate bru = git.getRefDatabase().newBatchUpdate();
-      try (ObjectInserter oi = git.newObjectInserter()) {
-        for (RevisionData revisionData : revisionsData) {
-
-          ObjectId newObjectID = null;
-          RevisionObjectData commitObject = revisionData.getCommitObject();
-
-          if (commitObject != null) {
-            RevCommit commit = RevCommit.parse(commitObject.getContent());
-            for (RevCommit parent : commit.getParents()) {
-              if (!git.getObjectDatabase().has(parent.getId())) {
-                throw new MissingParentObjectException(name, refSpec.getSource(), parent.getId());
-              }
-            }
-
-            StringBuffer error = new StringBuffer();
-            if (!ChangeMetaCommitValidator.isValid(
-                git, refSpec.getSource(), commit, error::append)) {
-              throw new MissingLatestPatchSetException(name, refSpec.getSource(), error.toString());
-            }
-          }
-
-          for (RevisionObjectData rev : revisionData.getBlobs()) {
-            ObjectId blobObjectId = oi.insert(rev.getType(), rev.getContent());
-            if (newObjectID == null) {
-              newObjectID = blobObjectId;
-            }
-            refHead = newObjectID;
-          }
-
-          if (commitObject != null) {
-            RevisionObjectData treeObject = revisionData.getTreeObject();
-            oi.insert(treeObject.getType(), treeObject.getContent());
-
-            refHead = oi.insert(commitObject.getType(), commitObject.getContent());
-          }
-
-          oi.flush();
-
-          if (commitObject == null) {
-            // Non-commits must be forced as they do not have a graph associated
-            bru.setAllowNonFastForwards(true);
-          }
-        }
-
-        ObjectId oldObjectId =
-            Optional.fromNullable(git.exactRef(refSpec.getSource()))
-                .transform(Ref::getObjectId)
-                .or(ObjectId.zeroId());
-        bru.addCommand(new ReceiveCommand(oldObjectId, refHead, refSpec.getSource()));
-        RefUpdateUtil.executeChecked(bru, git);
-        return new BatchRefUpdateState(bru);
-      }
     } catch (RepositoryNotFoundException e) {
       throw new ResourceNotFoundException(IdString.fromDecoded(name.get()), e);
     }
