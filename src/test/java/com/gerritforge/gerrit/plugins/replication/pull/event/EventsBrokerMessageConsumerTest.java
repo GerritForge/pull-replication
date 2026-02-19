3b13b45205b48e34acda2245163d032df2bfe702
Antonio Barone <syntonyze@gmail.com>
2026-02-19 17:52:35 +0100

    Leverage new events-broker API
    Receive events via the new receiveAsync api exposed by the
    events-broker.
    
    This allows to ack explicitly on message consumption rather than relying
    on auto commit being enabled.
    
    For the time being we ack() blindly (unless an exception occurs) once
    the event is received.
    
    The current change is merely for obtaining a green build and adapting to
    the new API interface; however, it is clearly not suitable because
    acking needs to happen upon completion of the operation, which at the
    moment happens asynchronously.
    
    In the follow-up change(s), the sync receiver would need to have a
    different logic and including the consumption and acking of replication
    events from a unique main loop.
    
    Bug: Issue 454827132
    Change-Id: I7eef8eaee9c646c8e1c16a3cec68cd01eb06cee9

--- src/test/java/com/gerritforge/gerrit/plugins/replication/pull/event/EventsBrokerMessageConsumerTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/replication/pull/event/EventsBrokerMessageConsumerTest.java
@@ -20,8 +20,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.gerritforge.gerrit.eventbroker.MessageAcknowledgement;
 import com.gerritforge.gerrit.plugins.replication.pull.ShutdownState;
 import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.RefUpdatedEvent;
 import com.google.gerrit.server.permissions.PermissionBackendException;
 import org.junit.Before;
@@ -40,6 +42,8 @@ public class EventsBrokerMessageConsumerTest {
   EventsBrokerMessageConsumer objectUnderTest;
   ShutdownState shutdownState;
 
+  private static final MessageAcknowledgement<Event> NOOP_ACK = (ignore) -> {};
+
   @Before
   public void setup() {
     shutdownState = new ShutdownState();
@@ -51,19 +55,23 @@ public class EventsBrokerMessageConsumerTest {
   @Test
   public void shouldRethrowExceptionWhenFetchThrowsAuthException() throws Exception {
     doThrow(PermissionBackendException.class).when(eventListener).fetchRefsForEvent(any());
-    assertThrows(EventRejectedException.class, () -> objectUnderTest.accept(new RefUpdatedEvent()));
+    assertThrows(
+        EventRejectedException.class,
+        () -> objectUnderTest.accept(new RefUpdatedEvent(), NOOP_ACK));
   }
 
   @Test
   public void shouldRethrowExceptionWhenFetchThrowsPermissionBackendException() throws Exception {
     doThrow(PermissionBackendException.class).when(eventListener).fetchRefsForEvent(any());
-    assertThrows(EventRejectedException.class, () -> objectUnderTest.accept(new RefUpdatedEvent()));
+    assertThrows(
+        EventRejectedException.class,
+        () -> objectUnderTest.accept(new RefUpdatedEvent(), NOOP_ACK));
   }
 
   @Test
   public void shouldNotThrowExceptionWhenFetchSucceed() throws Exception {
     doNothing().when(eventListener).fetchRefsForEvent(any());
-    objectUnderTest.accept(new RefUpdatedEvent());
+    objectUnderTest.accept(new RefUpdatedEvent(), NOOP_ACK);
   }
 
   @Test
@@ -73,7 +81,7 @@ public class EventsBrokerMessageConsumerTest {
 
     shutdownState.setIsShuttingDown(true);
 
-    objectUnderTest.accept(new RefUpdatedEvent());
+    objectUnderTest.accept(new RefUpdatedEvent(), NOOP_ACK);
     verify(eventsBroker, times(1)).disconnect("topicName", null);
   }
 }
