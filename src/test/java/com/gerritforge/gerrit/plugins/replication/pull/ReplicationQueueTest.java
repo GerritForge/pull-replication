c35d72d6653a2fdf5e2c2596328a3fe5a02ce85e
Daniele Sassoli <danielesassoli@gmail.com>
2026-07-10 17:07:24 +0100

    Correctly order refs in batch-apply-object
    It used to be that the meta refs were always sent last in the batch,
    however, if the batch also contains heads and we advance their pointer
    as a result of the batch being partially applied, then the heads might
    contain objects for which the corresponding change that brought them is
    inaccessible.
    
    Order the refs based on the following priority:
    - patchsets
    - meta refs
    - everything else (heads, tags, etc...)
    
    This ensures that commits referenced by branches have an associated
    navigable change.
    
    Bug: Issue 540020492
    Change-Id: Ib7f20495ad8a7fd7af31068b264cc3a56e990b8c

--- src/test/java/com/gerritforge/gerrit/plugins/replication/pull/ReplicationQueueTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/replication/pull/ReplicationQueueTest.java
@@ -274,20 +274,26 @@ public class ReplicationQueueTest {
   }
 
   @Test
-  public void shouldCallSendObjectReorderingRefsHavingMetaAtTheEnd() throws Exception {
-    Event event = generateBatchRefUpdateEvent("refs/changes/01/1/meta", "refs/changes/01/1/1");
+  public void shouldCallSendObjectReorderingRefs() throws Exception {
+    Event event =
+        generateBatchRefUpdateEvent(
+            "refs/heads/main",
+            "refs/changes/01/1/meta",
+            "refs/tags/v1.0",
+            "refs/notes/foo",
+            "refs/meta/config",
+            "refs/changes/01/1/1");
     objectUnderTest.start();
     objectUnderTest.onEvent(event);
-    verifySendObjectOrdering("refs/changes/01/1/1", "refs/changes/01/1/meta");
+    verifySendObjectOrdering(
+        "refs/changes/01/1/1",
+        "refs/changes/01/1/meta",
+        "refs/heads/main",
+        "refs/tags/v1.0",
+        "refs/meta/config",
+        "refs/notes/foo");
   }
 
-  @Test
-  public void shouldCallSendObjectKeepingMetaAtTheEnd() throws Exception {
-    Event event = generateBatchRefUpdateEvent("refs/changes/01/1/1", "refs/changes/01/1/meta");
-    objectUnderTest.start();
-    objectUnderTest.onEvent(event);
-    verifySendObjectOrdering("refs/changes/01/1/1", "refs/changes/01/1/meta");
-  }
 
   @Test
   public void shouldNotCallInitProjectWhenReplicateNewRepositoriesNotSet() throws Exception {
@@ -680,13 +686,14 @@ public class ReplicationQueueTest {
     return event;
   }
 
-  private void verifySendObjectOrdering(String firstRef, String secondRef) throws Exception {
+  private void verifySendObjectOrdering(String... expectedRefs) throws Exception {
     verify(fetchRestApiClient)
         .callBatchSendObject(any(), batchRefsCaptor.capture(), anyLong(), any());
     List<BatchApplyObjectData> batchRefs = batchRefsCaptor.getValue();
 
-    assertThat(batchRefs.get(0).refName()).isEqualTo(firstRef);
-    assertThat(batchRefs.get(1).refName()).isEqualTo(secondRef);
+    assertThat(batchRefs.stream().map(BatchApplyObjectData::refName).collect(Collectors.toList()))
+        .containsExactlyElementsIn(expectedRefs)
+        .inOrder();
   }
 
   private class TestEvent extends RefUpdatedEvent {
