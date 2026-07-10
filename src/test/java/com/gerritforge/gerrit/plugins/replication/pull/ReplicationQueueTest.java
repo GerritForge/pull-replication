78a11f26207bb9faabf0c5f030ed0b9a7201cb15
Daniele Sassoli <danielesassoli@gmail.com>
2026-07-10 17:07:24 +0100

    Correctly order refs in batch-apply-object
    It used to be that the meta refs were always sent last in the batch,
    however, if the batch also contains heads and we advance their pointer
    as a result of the batch being partially applied, then the heads might
    contain objects for which the corresponding change that brought them is
    inaccessible.
    
    Order the refs based on the following priority:
    - patchsets
    - meta refs
    - everything else (heads, tags, etc...)
    
    This ensures that commits referenced by branches have an associated
    navigable change.
    
    Bug: Issue 540020492
    Change-Id: Ib7f20495ad8a7fd7af31068b264cc3a56e990b8c

--- src/test/java/com/gerritforge/gerrit/plugins/replication/pull/ReplicationQueueTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/replication/pull/ReplicationQueueTest.java
@@ -290,6 +290,27 @@ public class ReplicationQueueTest {
   }
 
   @Test
+  public void shouldCallSendObjectReorderingRefs() throws Exception {
+    Event event =
+        generateBatchRefUpdateEvent(
+            "refs/heads/main",
+            "refs/changes/01/1/meta",
+            "refs/tags/v1.0",
+            "refs/notes/foo",
+            "refs/meta/config",
+            "refs/changes/01/1/1");
+    objectUnderTest.start();
+    objectUnderTest.onEvent(event);
+    verifySendObjectOrdering(
+        "refs/changes/01/1/1",
+        "refs/changes/01/1/meta",
+        "refs/heads/main",
+        "refs/tags/v1.0",
+        "refs/meta/config",
+        "refs/notes/foo");
+  }
+
+  @Test
   public void shouldNotCallInitProjectWhenReplicateNewRepositoriesNotSet() throws Exception {
     Event event = new TestEvent("refs/changes/01/1/meta");
 
@@ -680,13 +701,14 @@ public class ReplicationQueueTest {
     return event;
   }
 
-  private void verifySendObjectOrdering(String firstRef, String secondRef) throws Exception {
+  private void verifySendObjectOrdering(String... expectedRefs) throws Exception {
     verify(fetchRestApiClient)
         .callBatchSendObject(any(), batchRefsCaptor.capture(), anyLong(), any());
     List<BatchApplyObjectData> batchRefs = batchRefsCaptor.getValue();
 
-    assertThat(batchRefs.get(0).refName()).isEqualTo(firstRef);
-    assertThat(batchRefs.get(1).refName()).isEqualTo(secondRef);
+    assertThat(batchRefs.stream().map(BatchApplyObjectData::refName).collect(Collectors.toList()))
+        .containsExactlyElementsIn(expectedRefs)
+        .inOrder();
   }
 
   private class TestEvent extends RefUpdatedEvent {
